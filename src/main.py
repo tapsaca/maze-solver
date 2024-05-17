45d85f655bcbd1282a766f15c69bdb554fdaeb5f
tapsaca <98756983+tapsaca@users.noreply.github.com>
2024-05-17 17:33:51 +0300

    Update main function

--- src/main.py
+++ src/main.py
@@ -3,24 +3,24 @@ from graphics import Window
 from maze import Maze
 
 def main():
-    num_rows = 12
-    num_cols = 16
+    num_rows = 10
+    num_cols = 10
     margin = 50
     screen_x = 800
     screen_y = 600
-    cell_size_x = (screen_x - 2 * margin) / num_cols
-    cell_size_y = (screen_y - 2 * margin) / num_rows
+    cell_size_x = 50
+    cell_size_y = 50
 
     sys.setrecursionlimit(10000)
-    win = Window(screen_x, screen_y)
+    window = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, window)
     print("Maze created")
     is_solveable = maze.solve()
     if not is_solveable:
         print("Maze can not be solved!")
     else:
         print("Maze solved!")
-    win.wait_for_close()
+    window.wait_for_close()
 
 main()
